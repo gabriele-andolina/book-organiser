0f22a35b218448ed3d09552813143da9dd77de1c
gabriele-andolina <gabe.andolina@gmail.com>
2023-10-17 13:22:02 +0200

    Components: Created Main

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import "./style.css";
 
 function App() {
-    return <Header />;
+    return (
+        <div>
+            <Header />
+            <Main />
+        </div>
+    );
 }
 
 function Header() {
     return (
         <div className="page-title">
             <h1>Books for a Lifetime</h1>
+        </div>
+    );
+}
+
+function Main() {
+    return (
+        <div className="main-page">
+            <h2>I'm the main page</h2>
         </div>
     );
 }
